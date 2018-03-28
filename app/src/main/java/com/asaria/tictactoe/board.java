f9afc9c2005cf2f6d4451284ba42dd1788ac89aa
Kevin Asaria <asariakevin@gmail.com>
2018-03-28 12:28:10 +0300

    made the layou for the two grids

---
+++ app/src/main/java/com/asaria/tictactoe/board.java
@@ -0,0 +1,13 @@
+package com.asaria.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class board extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
